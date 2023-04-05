1b6d97ba6e473259eb772a1de8b652ae33df0171
PNguyen230 <pn784648@gmail.com>
2023-04-05 14:00:30 +0700

    First commit

---
+++ js/javasripct.js
@@ -0,0 +1,18 @@
+function tong(n)
+        {
+            let tam = 0;
+            for(i = 0; i <= n; i++)
+            {
+                tam += i;
+            }
+            return tam
+        }
+function tich(n)
+        {
+            let tam = 0;
+            for(i = 0; i <= n; i++)
+            {
+                tam *= i;
+            }
+            return tam
+        }
